36f8171687ba8c12409c5aca876c4539c7814c3f
Greg Church <greggypc@users.noreply.github.com>
2018-01-07 11:27:39 -0600

    keys hidden

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.cK7M2gwXCD2aF0BbzYNVSoM6g,
-  consumer_secret: process.env.	5z7K1pGozhL32NHj73FOrds3T8Rh47WMgpeLGscJT3jw5elNRN,
-  access_token_key: process.env.389211895-Ax3hVyLJEhrO1AD4iWwHC4htl4mkcKyBMrVxUkFC,
-  access_token_secret: process.env.EWbiYMMDh4S9YV4CqSQUzv4wam563VTgCHio91WlFHTNB
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
