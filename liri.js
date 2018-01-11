3451738b5b2edc590546163e4f32b704ec567bce
Greg Church <greggypc@users.noreply.github.com>
2018-01-10 21:08:07 -0600

    saved dependencies. stated all requirements. woking on taking twitter handle from user and displaying 20 tweets

--- liri.js
+++ liri.js
@@ -5,24 +5,81 @@ require("dotenv").config();
 //import keys.js file (twitter and spotify keys) and store it in a variable
 var keys = require("./keys.js");
 
-//access your keys information like so
-//var spotify = new Spotify(keys.spotify);
-//var client = new Twitter(keys.twitter);
-
-//create variables that will take commands from user
-//var myTweets = process.argv[2];
-console.log('about to run tweet if statement');
+//require request
 var request = require("request");
 
-if (process.argv[2] === "my-tweets") {
-	request("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=greggypc&count=20", function(error, response, body) {
-console.log('inside of tweet if statement');
-  // If the request was successful...
-  if (!error && response.statusCode === 200) {
-  // Then log the body from the site!
-  console.log(JSON.parse(body).text);
-  }
-});
+//require inquirer
+var inquirer = require("inquirer");
+
+//require file system
+var fs = require('fs');
+
+//require spotify and twitter packages
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+
+//access your keys information like so
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+//create variables that will take commands from user
+var command = process.argv[2];
+
+//accept commands from user and fire appropriate function
+switch (command) {
+  case "my-tweets":
+    console.log("user command: my-tweets");
+    tweets();
+    break;
+  case "spotify-this-song":
+    console.log("user command: spotify");
+    spotify();
+    break;
+  case "movie-this":
+    console.log("user command: movie");
+    movie();
+    break;
+  case "do-what-it-says":
+    console.log("user command: do something");
+    doSomething();
+    default:
+    console.log("not an option");
 }
 
+//return latest 20 tweets from user input twitter handle
+function tweets() {
+	inquirerTweets();
+	console.log(name);
+    client.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=TheDailyShow&count=20", function(error, response, tweet) {
+        console.log('inside of function tweet');
+        // If the request was successful...
+        if (!error && response.statusCode === 200) {
+            // Log the last 20 tweets
+            console.log(JSON.parse(tweet).text);
+            console.log(tweet);
+        }
+    });
+}
+
+function inquirerTweets(handle) {
+  inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "Which twitter handle would you like to see?",
+      name: "handle"
+    }
+   ])
+
+  .then(function(inquirerResponse) {
+    // return twitter handle
+    if (err){console.log(err);} 
+    else if (inquirerResponse.name != null) {
+    	console.log(inquirerResponse.name);
+      return inquirerResponse.name;
+    }
+  });
   
+}
+
